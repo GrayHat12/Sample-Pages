dfa4b64babf17af1d55e2d19ec9c1944601c30c1
GrayHat12 <grayhathacks10@gmail.com>
2020-06-05 02:19:42 +0530

    fulfilled the basic requirements
    open to improvements

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React from "react";
 import SamplePage from "./pages/samplePage";
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import BottomNavigation from "@material-ui/core/BottomNavigation";
 import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
 import HomeIcon from "@material-ui/icons/Home";
@@ -16,16 +12,29 @@ import P2 from "./pages/p2";
 import P3 from "./pages/p3";
 import P4 from "./pages/p4";
 
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
+import { Button } from "@material-ui/core";
+import Brightness4Icon from '@material-ui/icons/Brightness4';
+
 class App extends React.Component {
   state = {
     value: 0,
     props: null,
+    drawer: false,
     mode: "light",
   };
   constructor(props) {
     super(props);
     this.onChange = this.onChange.bind(this);
     this.setProps = this.setProps.bind(this);
+    this.onDrawerClose = this.onDrawerClose.bind(this);
+    this.openDrawer = this.openDrawer.bind(this);
+    this.changeMode = this.changeMode.bind(this);
   }
   onChange(event, newValue) {
     this.setState({
@@ -36,18 +45,70 @@ class App extends React.Component {
     if (newValue === 2) this.state.props.history.push("/p3");
     if (newValue === 3) this.state.props.history.push("/p4");
   }
+  changeMode(event){
+    if (this.state.mode==='light') this.setState({mode: 'dark'});
+    else this.setState({mode : 'light'});
+    if (event) event.preventDefault();
+  }
   setProps(props) {
     if (this.state.props != null) return;
     this.setState({
       props: props,
     });
   }
+  onDrawerClose() {
+    this.setState({
+      drawer: false,
+    });
+  }
+  openDrawer(event) {
+    if (this.state.drawer) return;
+    this.setState({
+      drawer: true,
+    });
+    if (event) event.preventDefault();
+  }
   render() {
     return (
       <Router>
+        <AppBar
+          position="static"
+          className={"appBar " + this.state.mode}
+        >
+          <Toolbar variant="dense">
+            <IconButton
+              edge="start"
+              onClick={this.openDrawer}
+              className={"menuButtonClass " + this.state.mode}
+              color="inherit"
+              aria-label="menu"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" color="inherit">
+              APP NAME
+            </Typography>
+            <IconButton onClick={this.changeMode}>
+              <Brightness4Icon/>
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        <SwipeableDrawer
+          anchor="left"
+          className={"drawer " + this.state.mode}
+          open={this.state.drawer}
+          onOpen={this.openDrawer}
+          onClose={this.onDrawerClose}
+        >
+          <span className={"drawerItem " + this.state.mode}>ITEM 1</span>
+          <span className={"drawerItem " + this.state.mode}>ITEM 1</span>
+          <span className={"drawerItem " + this.state.mode}>ITEM 1</span>
+        </SwipeableDrawer>
         <div className={"pageClass " + this.state.mode}>
           <Switch>
-            <Route exact path="/"
+            <Route
+              exact
+              path="/"
               render={(props) => {
                 this.setProps(props);
                 return (
--- src/pages/samplePage.js
+++ src/pages/samplePage.js
@@ -1,11 +1,184 @@
-import React from 'react';
-import '../css/SamplePage.css';
+import React from "react";
+import "../css/SamplePage.css";
+//import Drawer from "@material-ui/core/Drawer";
+import Fab from '@material-ui/core/Fab';
+import NavigateNextIcon from "@material-ui/icons/NavigateNext";
 
 class SamplePage extends React.Component {
-    render(){
-        return(<div className="sample light">
-            HELLO
-        </div>);
+  state = {
+    drawer: false,
+    q1: 1,
+    labels: ["Breakfast", "Lunch", "Dinner"],
+    defaultTime: "1200am",
+    q2: {
+      selectedOption: 1,
+    },
+    q3: 3,
+  };
+  constructor(props) {
+    super(props);
+    this.getBanners = this.getBanners.bind(this);
+    this.onInputChangeQ1 = this.onInputChangeQ1.bind(this);
+    this.onInputChangeQ3 = this.onInputChangeQ3.bind(this);
+  }
+  getBanners() {
+    if (this.state.q1 === 0) return null;
+    var list = [];
+    var items = [];
+    if (this.state.q1 === 1) items = this.state.labels.slice(0, 2);
+    if (this.state.q1 === 2) items = [this.state.labels[2]];
+    if (this.state.q1 === 3) items = this.state.labels;
+    for (var i = 0; i < items.length; i++) {
+      var item = (
+        <div key={i} className={"banner " + this.props.mode}>
+          <span>{items[i]}</span>
+          <span style={{ fontSize: "xx-small" }}>{this.state.defaultTime}</span>
+        </div>
+      );
+      list.push(item);
     }
+    return list;
+  }
+  onInputChangeQ1(event) {
+    if (event.target.value === "Breakfast & Lunch") this.setState({ q1: 1 });
+    if (event.target.value === "Dinner") this.setState({ q1: 2 });
+    if (event.target.value === "Both") this.setState({ q1: 3 });
+    event.persist();
+  }
+  onInputChangeQ3(event) {
+    if (event.target.value === "Male") this.setState({ q3: 1 });
+    if (event.target.value === "Female") this.setState({ q3: 2 });
+    if (event.target.value === "Awesome") this.setState({ q3: 3 });
+    event.persist();
+  }
+  render() {
+    return (
+      <div className={"sample " + this.props.mode}>
+        <div className={"mainContent " + this.props.mode}>
+          <div className={"questionGroup " + this.props.mode}>
+            <div className={"questionHead " + this.props.mode}>
+              <span>How Many Meals ?</span>
+            </div>
+            <div className="group">
+              <input
+                className="questionOption"
+                value="Breakfast & Lunch"
+                onChange={(event) => {}}
+                onClick={this.onInputChangeQ1}
+                checked={this.state.q1 === 1}
+                type="radio"
+              />
+              <label className="questionOptionLabel">Breakfast & Lunch</label>
+            </div>
+            <div
+              className="group"
+              onClick={this.onInputChangeQ1}
+              value="Dinner"
+            >
+              <input
+                className="questionOption"
+                value="Dinner"
+                onChange={(event) => {}}
+                onClick={this.onInputChangeQ1}
+                checked={this.state.q1 === 2}
+                type="radio"
+              />
+              <label className="questionOptionLabel">Dinner</label>
+            </div>
+            <div className="group">
+              <input
+                className="questionOption"
+                onChange={(event) => {}}
+                value="Both"
+                onClick={this.onInputChangeQ1}
+                checked={this.state.q1 === 3}
+                type="radio"
+              />
+              <label className="questionOptionLabel">Both</label>
+            </div>
+          </div>
+          <div className={"questionGroup " + this.props.mode}>
+            <div className={"questionHead " + this.props.mode}>
+              <span>How Many Members ?</span>
+            </div>
+            <select
+              defaultValue={this.state.q2.selectedOption}
+              className={"questionSelect " + this.props.mode}
+            >
+              <option className={"questionSelectOption " + this.props.mode}>
+                1
+              </option>
+              <option className={"questionSelectOption " + this.props.mode}>
+                2
+              </option>
+              <option className={"questionSelectOption " + this.props.mode}>
+                3
+              </option>
+              <option className={"questionSelectOption " + this.props.mode}>
+                4
+              </option>
+              <option className={"questionSelectOption " + this.props.mode}>
+                5
+              </option>
+              <option className={"questionSelectOption " + this.props.mode}>
+                6
+              </option>
+              <option className={"questionSelectOption " + this.props.mode}>
+                7
+              </option>
+            </select>
+          </div>
+          <div className={"questionGroup " + this.props.mode}>
+            <div className={"questionHead " + this.props.mode}>
+              <span>Preferred gender of Cook ?</span>
+            </div>
+            <div className="group">
+              <input
+                className="questionOption"
+                value="Male"
+                onChange={(event) => {}}
+                onClick={this.onInputChangeQ3}
+                checked={this.state.q3 === 1}
+                type="radio"
+              />
+              <label className="questionOptionLabel">Male</label>
+            </div>
+            <div className="group">
+              <input
+                className="questionOption"
+                value="Female"
+                onChange={(event) => {}}
+                onClick={this.onInputChangeQ3}
+                checked={this.state.q3 === 2}
+                type="radio"
+              />
+              <label className="questionOptionLabel">Female</label>
+            </div>
+            <div className="group">
+              <input
+                className="questionOption"
+                value="Awesome"
+                onChange={(event) => {}}
+                onClick={this.onInputChangeQ3}
+                checked={this.state.q3 === 3}
+                type="radio"
+              />
+              <label className="questionOptionLabel">
+                Just An Awesome Cook
+              </label>
+            </div>
+          </div>
+          <div className="bottomPane">
+            <div className={"banners " + this.props.mode}>
+              {this.getBanners()}
+            </div>
+            <Fab color="primary" size="small" className={"nextButton " + this.props.mode}>
+              <NavigateNextIcon />
+            </Fab>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 export default SamplePage;
